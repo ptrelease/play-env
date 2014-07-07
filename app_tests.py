1d9b3f8ff8c9f95a3fba2a710c2176a94ab7d46f
treacle74 <ptrelease@gmail.com>
2014-07-07 16:12:07 +0100

    added some Travis tests

--- app_tests.py
+++ app_tests.py
@@ -1,1 +1,20 @@
 #no tests
+
++import app
+ +import unittest
+ +
+ +class AppTestCase(unittest.TestCase):
+ +
+ +    def setUp(self):
+ +        app.app.config['TESTING'] = True
+ +        self.app = app.app.test_client()
+ +
+ +    #Nothing to put into the teardown for this testcase
+ +    #def tearDown(self):
+ +
+ +    def test_hello_world(self):
+ +        rv = self.app.get('/')
+ +        assert 'Hello Paul!' in rv.data
+ +
+ +if __name__ == '__main__':
+ +    unittest.main()
